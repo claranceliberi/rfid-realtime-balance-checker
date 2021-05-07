367b5ea6fcdaeea0a8794dd89cbf997f809acd8a
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-07 16:03:52 +0200

    feat: transaction controller

--- src/controllers/card.controller.ts
+++ src/controllers/card.controller.ts
@@ -18,7 +18,7 @@ class cardController{
 
     async getAll(req:Request, res:Response){
         try{
-            const result = await this.prisma.card.findMany({where:{}})
+            const result = await this.prisma.card.findMany({include:{transaction:true}})
             res.json(result)
         }catch (e) {
             res.status(500).json({msg:"error",error:e})
---
+++ src/controllers/transaction.controller.ts
@@ -0,0 +1,36 @@
+import {Request, Response} from "express";
+
+class transactionController{
+     prisma
+
+    constructor(prisma) {
+        this.prisma = prisma
+    }
+
+    async create(req:Request, res:Response){
+      try{
+        const result = await this.prisma.card.create({data: {...req.body}})
+        res.json(result)
+      }catch (e){
+        res.status(500).json({msg:"error",error:e})
+      }
+    }
+
+    async getAll(req:Request, res:Response){
+        try{
+            const result = await this.prisma.card.findMany({include:{card:true}})
+            res.json(result)
+        }catch (e) {
+            res.status(500).json({msg:"error",error:e})
+        }
+    }
+
+    async getByCard(req:Request, res:Response){
+        try{
+            const result = await this.prisma.card.findMany({where:{card_UUID:req.params.id}, include:{card:true}})
+            res.json(result)
+        }catch (e) {
+            res.status(500).json({msg:"error",error:e})
+        }
+    }
+}
