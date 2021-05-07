a2b370876e9212782628d14eaca7e4b60f260c06
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-07 19:23:24 +0200

    transaction compoennt

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,14 @@
+<script lang="ts" setup>
+import Transactions from "./components/Transactions.vue";
+
+
+</script>
+
 <template>
 <div>
-  <ul v-for="transaction in transactions" :key="transactions.card_UUID+transactions.created_at">
-    <li>{{transaction.card_UUID}}</li>
-  </ul>
+  <div class="transactions">
+    <Transactions />
+  </div>
 </div>
 </template>
 
-<script lang="ts" setup>
-import {ref} from 'vue'
-
-const transactions:object = ref([])
-
-
-function fetchTransaction(){
-  const link = `http://${location.hostname}:5000/transactions`
-
-  fetch(link)
-  .then(d => {
-    d.json().then(d => {
-      transactions.value = d
-    }).catch(e => {
-      console.log(e)
-    })
-  }).catch(e => {
-    console.log(e)
-  })
-}
-
-fetchTransaction()
-
-</script>
