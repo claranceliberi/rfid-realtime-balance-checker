1d9de03ffdf1d7ed72b26d54caceff3d38c68224
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-07 18:50:43 +0200

    fix: cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 import * as bodyParser from 'body-parser'
 import express from 'express'
+import cors from 'cors'
 
 const prisma = new PrismaClient()
 const app = express()
 
 app.use(bodyParser.json())
+app.use(cors())
+
+
 
 app.post(`/cards`, async (req, res) => {
   try{
