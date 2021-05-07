638a28f23e197549d67ca278e957d2d9405e5197
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-07 16:25:34 +0200

    feat: card routes

--- src/routes/card.routes.ts
+++ src/routes/card.routes.ts
@@ -1,1 +1,19 @@
 
+import {Express} from "express";
+import { PrismaClient } from '@prisma/client'
+import cardController from "../controllers/card.controller";
+
+class cardRoutes{
+    app : Express
+    prisma = new PrismaClient()
+
+    constructor(app) {
+        this.app = app
+        const card = new cardController(this.prisma.transaction)
+
+        this.app.get('/',card.getAll)
+        this.app.post('/',card.create)
+    }
+}
+
+export default cardRoutes
