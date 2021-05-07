ca660380c82bf354056d00bc1475fd65196230b8
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-07 18:50:06 +0200

    feat: receiving data from backend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,5 +5,27 @@
 </template>
 
 <script lang="ts" setup>
+import {ref} from 'vue'
+
+const data:object = ref({})
+
+
+function fetchTransaction(){
+  const link = `http://${location.hostname}:5000/transactions`
+
+  fetch(link)
+  .then(d => {
+    d.json().then(d => {
+      data.value = d
+    }).catch(e => {
+      console.log(e)
+    })
+  }).catch(e => {
+    console.log(e)
+  })
+}
+
+fetchTransaction()
+
 
 </script>
