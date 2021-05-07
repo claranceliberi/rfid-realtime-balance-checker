7ff08df7d4bb443da2a309a8e17d1e658a44ee86
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-07 15:50:27 +0200

    feat: car controller

---
+++ src/controllers/card.controller.ts
@@ -0,0 +1,27 @@
+import {Request, Response} from "express";
+
+class cardController{
+     prisma
+
+    constructor(prisma) {
+        this.prisma = prisma
+    }
+    
+    async create(req:Request, res:Response){
+      try{
+        const result = await this.prisma.card.create({data: {...req.body}})
+        res.json(result)
+      }catch (e){
+        res.status(500).json({msg:"error",error:e})
+      }
+    }
+
+    async getAll(req:Request, res:Response){
+        try{
+            const result = await this.prisma.card.findMany({where:{}})
+            res.json(result)
+        }catch (e) {
+            res.status(500).json({msg:"error",error:e})
+        }
+    }
+}
